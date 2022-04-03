bdf08c8917d6776ab214d12de762cbc8a664fecd
BobrikAU <kirbob0@yandex.ru>
2022-04-03 21:29:18 +0300

    refactoring: переделан переход на темную тему для отдельных элементов

---
+++ scripts/develop_link_in_blocks.js
@@ -0,0 +1,8 @@
+const root = document.querySelector(':root');
+
+
+
+
+root.style.setProperty('--colorTextMain', '#fff')
+root.style.setProperty('--opacityLinkHover', '65%'); // меняет уровень opacity для ссылок в блоке на открытую страницу на уровень для темной темы
+root.style.setProperty('--opacityLinkClosedSlideHover', '50%');  // меняет уровень opacity для ссылок в блоке на закрытую страницу на уровень для темной темы
